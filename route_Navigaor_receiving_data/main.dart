d9f4ab7daba1e46123ea650af333abb063baf625
Abell-Choi <trooperalex92@gmail.com>
2022-05-20 16:42:45 +0900

    change stateless -> stateful widget

--- route_Navigaor_receiving_data/main.dart
+++ route_Navigaor_receiving_data/main.dart
@@ -1,54 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: HoemScreen()));
+void main() => runApp(MaterialApp(home: HomeScreen()));
 
-// 홈 스크린 구현
-class HoemScreen extends StatelessWidget {
-  const HoemScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String data = 'not complite';
+
+  _navigation_System(BuildContext context) async {
+    final res = await Navigator.push(
+        context, MaterialPageRoute(builder: (context) => SecondSCreen()));
+    setState(() {
+      data = res;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SelectionButton(),
+        child: Container(
+            child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                _navigation_System(context);
+              },
+              child: Text('Run'),
+            ),
+            Text('$data'),
+          ],
+        )),
       ),
     );
   }
 }
 
-// 선택창 구현
-class SelectionButton extends StatelessWidget {
-  const SelectionButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _navigateAndDisplaySelection(context);
-      },
-      child: Text('pick an option, any option!'),
-    );
-  }
-
-  // SelectionScreen 띄우고 pop으로 부터 결과를 기다리는 메서드
-  _navigateAndDisplaySelection(BuildContext context) async {
-    // Navigator.push -> Future 반환
-    // Navigator pop이 호출된 이후를 완성함
-    final res = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: ((context) => SelectionScreen())),
-    );
-
-    // 결과가 있을 경우 snackbar로 호출
-    ScaffoldMessenger.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text('$res')));
-  }
-}
-
-// 선택 창
-class SelectionScreen extends StatelessWidget {
-  const SelectionScreen({super.key});
+class SecondSCreen extends StatelessWidget {
+  const SecondSCreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -57,15 +51,11 @@ class SelectionScreen extends StatelessWidget {
         child: Column(
           children: [
             ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context, 'OK');
-                },
-                child: Text('OK')),
+                onPressed: () => {Navigator.pop(context, 'ok')},
+                child: Text('ok')),
             ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context, 'NO');
-                },
-                child: Text('NO')),
+                onPressed: () => {Navigator.pop(context, 'no')},
+                child: Text('no')),
           ],
         ),
       ),
