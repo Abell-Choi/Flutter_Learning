1c31fd51ec4d23355be8d816d887de6a6922117b
Abell_Choi <trooperalex92@gmail.com>
2022-05-30 12:30:10 +0900

    Update 1_http_default_connecting.dart
    simple getting web doms

--- http/1_http_default_connecting.dart
+++ http/1_http_default_connecting.dart
@@ -1,35 +1,19 @@
 import 'package:flutter/material.dart';
-import 'dart:convert'; // 한글 깨짐 방지
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() async {
-  var url = Uri.https('naver.com', '/');
-  var res = await http.get(url, headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  });
-  var stcode = res.statusCode;
-  var resHeader = res.headers;
-  var resBody = utf8.decode(res.bodyBytes);
-
-  print('states : $stcode');
-  print('resHeader : $resHeader');
-  print('resBody : $resBody');
-
-  runApp(MaterialApp(
-    home: Test(),
-  ));
+Future<http.Response> fetchPost() {
+  return http.get(Uri.parse('www.naver.com'));
 }
 
-class Test extends StatelessWidget {
-  const Test({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text('nobody'),
-      ),
-    );
+void main() async {
+  print('run server');
+  var res = await fetchPost();
+  if (res.statusCode == 200) {
+    print('res 200 code ok');
+  } else {
+    print('err');
   }
+
+  print(res.body);
 }
